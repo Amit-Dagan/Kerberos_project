f4246a147ed3468f9bb86ee34dc635c78b7f8ae2
amit <amitdagan78@gmail.com>
2024-01-26 15:07:49 +0200

    message is here

---
+++ client.py
@@ -0,0 +1,49 @@
+import socket
+import json
+
+# header = {
+#     "client id": 123132,
+#     "Code":1025,
+#     "Version":24,
+#     "paylode size":100
+#     }
+
+# paylode = {
+#       "name":"amit",
+#       "password":"asdasda"
+#    }
+
+# data = {
+#     "header": header,
+#     "paylode": paylode
+# }
+
+
+# print(data)
+
+#---------------------------------
+
+# mydata = data
+
+# code = mydata["header"]["Code"]
+
+# if code == 1024:
+#     print
+
+
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.connect(('10.0.0.19', 1234))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    s.close()
+
---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('10.0.0.19', 1234))
+s.listen(5)
+
+while True:
+    clientsocket, adress = s.accept()
+    print(f"Connection from {adress}")
+    clientsocket.send(bytes("Welcom", "utf-8"))
