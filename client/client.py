bae99c5ccc48052bb2636e71aec2da634a691181
amit <amitdagan78@gmail.com>
2024-01-26 17:13:10 +0200

    now you can sign up

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
 import socket
 import json
+import os
+import uuid
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +14,42 @@ try:
 except Exception as e:
     print(f"An error occurred: {e}")
 
-finally:
-    s.close()
+
+name = ""
+id = ""
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+file_name = 'me.info'
+file_path = os.path.join(current_directory, file_name)
+    
+try:
+
+    f = open(file_path, 'r')
+    name = f.readline()
+    id = f.readline()
+    print(f"name = {name}id = {id}")
+except Exception as e:
+    print(e)
+
+    f = open(file_path, 'w')
+    name = input("Enter your full name: ")+'\n'
+    
+    s.send(bytes(name, "utf-8"))
+    msg = s.recv(1024)
+    f.write(name)
+    print(msg.decode("utf-8"))
+    f.write(msg.decode("utf-8"))
+
+
+
+
+
+s.close()
+f.close()
+
+
+
+
+
+
 
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import uuid
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('10.0.0.19', 1234))
@@ -6,7 +7,13 @@ s.listen(5)
 
 while True:
     clientsocket, adress = s.accept()
-    print(f"Connection from {adress}")
+    print(f"Connection from {adress}")    
     clientsocket.send(bytes("Welcom", "utf-8"))
+    msg = clientsocket.recv(1024)
+    id = uuid.uuid4()
+    id_str = str(id)
+    print(id)
+    clientsocket.send(bytes(id_str, "utf-8"))
+
 #change  
 ptint("asdasd")
