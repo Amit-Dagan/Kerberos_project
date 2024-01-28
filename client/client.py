1691bd9b3a93c1ea47b9bc54204616c30c070eec
amit <amitdagan78@gmail.com>
2024-01-28 16:00:21 +0200

    update

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import socket
 import json
 import os
 import uuid
-
+import hashlib
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -23,7 +23,7 @@ file_name = 'me.info'
 file_path = os.path.join(current_directory, file_name)
     
 try:
-
+    1/0
     f = open(file_path, 'r')
     name = f.readline()
     id = f.readline()
@@ -32,13 +32,34 @@ except Exception as e:
     print(e)
 
     f = open(file_path, 'w')
-    name = input("Enter your full name: ")+'\n'
+    # name = input("Enter your full name: ")
+    # password = input("Enter password: ")
+
+    hash = hashlib.sha256()
+    hash.update(b"{password}")
+    data = {
+        'Header': {
+            'Client ID': '',
+            'Version': 24,
+            'Code': 1024,
+            'Payload Size': 4
+        },
+        'Payload': {
+            'Name': name,
+            'Password': hash.digest
+        }
+    }
+
+    print(data)
     
-    s.send(bytes(name, "utf-8"))
-    msg = s.recv(1024)
-    f.write(name)
-    print(msg.decode("utf-8"))
-    f.write(msg.decode("utf-8"))
+    
+
+    # s.send(bytes(json.dumps(data), "utf-8"))
+    # msg = s.recv(1024)
+    # f.write(name)
+    # f.write('\n')
+    # print(msg.decode("utf-8"))
+    # f.write(msg.decode("utf-8"))
 
 
 
@@ -46,6 +67,9 @@ except Exception as e:
 
 s.close()
 f.close()
+
+
+
 
 
 
--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 import socket
 import uuid
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('10.0.0.19', 1234))
-s.listen(5)
 
-while True:
-    clientsocket, adress = s.accept()
-    print(f"Connection from {adress}")    
-    clientsocket.send(bytes("Welcom", "utf-8"))
-    msg = clientsocket.recv(1024)
-    id = uuid.uuid4()
-    id_str = str(id)
-    print(id)
-    clientsocket.send(bytes(id_str, "utf-8"))
 
-#change  
-ptint("asdasd")
+def __init__():
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('10.0.0.19', 1234))
+    s.listen(5)
+
+    while True:
+        clientsocket, adress = s.accept()
+        print(f"Connection from {adress}")    
+        clientsocket.send(bytes("Welcom", "utf-8"))
+        msg = clientsocket.recv(1024)
+        id = sign_up(msg)
+        id_str = str(id)
+        print(id)
+        clientsocket.send(bytes(id_str, "utf-8"))
+
+
+def sign_up(name):
+    #check if the name is in clients
+    return uuid.uuid4()
